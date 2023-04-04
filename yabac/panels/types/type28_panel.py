7cef7e62e54a02dfb34dba4e444b5d0de96332ba
Unleashed <shadowmahdi1998@gmail.com>
2023-04-04 19:44:06 +0300

    Bactype28 Fix and new Attachment Bones

--- yabac/panels/types/type28_panel.py
+++ yabac/panels/types/type28_panel.py
@@ -8,7 +8,7 @@ class Type28Panel(BasePanel):
         self.u_00 = self.add_hex_entry(self.unknown_page, 'U_00')
         self.u_040 = self.add_hex_entry(self.unknown_page, 'U_040')
         self.u_08 = self.add_hex_entry(self.unknown_page, 'U_08')
-        self.u_12 = self.add_hex_entry(self.unknown_page, 'U_12')
+        self.f_12 = self.add_float_entry(self.unknown_page, 'F_12')
         self.u_16 = self.add_hex_entry(self.unknown_page, 'U_16')
         self.u_20 = self.add_hex_entry(self.unknown_page, 'U_20')
         self.u_24 = self.add_hex_entry(self.unknown_page, 'U_24')
