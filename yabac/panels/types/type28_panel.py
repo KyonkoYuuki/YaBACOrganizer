49ddad34cdd12e4fcfa88433e78b97afce65eb67
Unleashed <shadowmahdi1998@gmail.com>
2023-04-07 00:06:44 +0300

    renames on type28 and 29

--- yabac/panels/types/type28_panel.py
+++ yabac/panels/types/type28_panel.py
@@ -5,10 +5,30 @@ class Type28Panel(BasePanel):
     def __init__(self, *args):
 
         BasePanel.__init__(self, *args)
-        self.u_00 = self.add_hex_entry(self.unknown_page, 'U_00')
+
+
+        self.dyt_flags = self.add_multiple_selection_entry(self.unknown_page, 'DYT Flags?', majorDimension=3, choices=[
+            (None, None, True),
+            (None, None, True),
+            (None, None, True),
+            (None, None, True),
+            ('Dyt Index', {
+                'DATA000?': 0x0,
+                'DATA001': 0x1,
+                'DATA002': 0x2,
+                'DATA003': 0x3,
+                'DATA004': 0x4,
+                'DATA005': 0x5,
+                'DATA006': 0x6,
+                'DATA007': 0x7,
+                'DATA008': 0x8
+            }, False)
+        ])
+
+
         self.u_040 = self.add_hex_entry(self.unknown_page, 'U_040')
-        self.u_08 = self.add_hex_entry(self.unknown_page, 'U_08')
-        self.f_12 = self.add_float_entry(self.unknown_page, 'F_12')
+        self.switch_transition_start = self.add_float_entry(self.unknown_page, 'Switch Transition (Start)')
+        self.switch_transition_end = self.add_float_entry(self.unknown_page, 'Switch Transition (End)')
         self.u_16 = self.add_hex_entry(self.unknown_page, 'U_16')
         self.u_20 = self.add_hex_entry(self.unknown_page, 'U_20')
         self.u_24 = self.add_hex_entry(self.unknown_page, 'U_24')
--- yabac/panels/types/type29_panel.py
+++ yabac/panels/types/type29_panel.py
@@ -1,22 +1,47 @@
-from yabac.panels.types import BasePanel
+from yabac.panels.types import BasePanel, Page
 
 
 class Type29Panel(BasePanel):
     def __init__(self, *args):
 
         BasePanel.__init__(self, *args)
-        self.u_08 = self.add_hex_entry(self.unknown_page, 'U_08')
+
+        #ean_page = Page(self.notebook)
+        #self.notebook.InsertPage(1, ean_page, 'Parts')
+
+
+        self.part_flags = self.add_multiple_selection_entry(self.unknown_page, 'PartFlags', majorDimension=3, choices=[
+            (None, None, True),
+            (None, None, True),
+            (None, None, True),
+            (None, None, True),
+            ('Part ID', {
+                'None?': 0x0,
+                'FaceBase': 0x1,
+                'FaceForhead': 0x2,
+                'FaceEye': 0x3,
+                'FaceNose': 0x4,
+                'Hair': 0x5,
+                'Bust': 0x6,
+                'Pants': 0x7,
+                'Rist': 0x8,
+                'Boots': 0x9
+            }, False)
+        ])
+
         self.u_12 = self.add_hex_entry(self.unknown_page, 'U_12')
 
 
-        self.f_16 = self.add_float_entry(self.unknown_page, 'f_16')
-        self.f_20 = self.add_float_entry(self.unknown_page, 'f_20')
-        self.f_24 = self.add_float_entry(self.unknown_page, 'f_24')
-        self.f_28 = self.add_float_entry(self.unknown_page, 'f_28')
-        self.f_32 = self.add_float_entry(self.unknown_page, 'f_32')
-        self.f_36 = self.add_float_entry(self.unknown_page, 'f_36')
-        self.f_40 = self.add_float_entry(self.unknown_page, 'f_40')
-        self.f_44 = self.add_float_entry(self.unknown_page, 'f_44')
+        self.glare_red_start = self.add_float_entry(self.unknown_page, 'Glare Red (Start)')
+        self.glare_green_start = self.add_float_entry(self.unknown_page, 'Glare Green (Start)')
+        self.glare_blue_start = self.add_float_entry(self.unknown_page, 'Glare Blue (Start)')
+        self.glare_alpha_start = self.add_float_entry(self.unknown_page, 'Glare Alpha (Start)')
+        self.glare_red_end = self.add_float_entry(self.unknown_page, 'Glare Red (End)')
+        self.glare_green_end = self.add_float_entry(self.unknown_page, 'Glare Green (End)')
+        self.glare_blue_end = self.add_float_entry(self.unknown_page, 'Glare Blue (End)')
+        self.glare_alpha_end = self.add_float_entry(self.unknown_page, 'Glare Alpha (End)')
+
+
         self.f_48 = self.add_float_entry(self.unknown_page, 'f_48')
 
         self.u_52 = self.add_hex_entry(self.unknown_page, 'u_52')
