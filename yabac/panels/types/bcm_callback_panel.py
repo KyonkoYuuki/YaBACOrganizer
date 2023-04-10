3007c743cf2b48eb75c297ceef0fc1789be3bdb7
Unleashed <shadowmahdi1998@gmail.com>
2023-04-10 22:40:59 +0300

    update bcmflags

--- yabac/panels/types/bcm_callback_panel.py
+++ yabac/panels/types/bcm_callback_panel.py
@@ -5,9 +5,10 @@ class BcmCallbackPanel(BasePanel):
     def __init__(self, *args):
         BasePanel.__init__(self, *args)
         self.bcm_link_flags = self.add_multiple_selection_entry(self.entry_page, 'BCM Link Flags', choices=[
-            ('Link Options #1', ['Ki blast', 'Jump', 'Guard', 'Step dash/fly'], True),
-            ('Link Options #2 / Reciver Link ID', ['Combo', 'Supers', 'Ultimates/Evasives', 'Z-Vanish'], True),
-            ('Link Options #3', ['Counters', None, None, 'Back-hit'], True),
-            ('Link Options #4', ['Attacks', 'Movement', 'Disable Ki blast', 'No links'], True)
+            ('Link Options #1', None, True),
+            ('Link Options #2', ['Ki blast', 'Jump', 'Guard', 'Step dash/fly'], True),
+            ('Link Options #3 / Reciver Link ID', ['Combo', 'Supers', 'Ultimates/Evasives', 'Z-Vanish'], True),
+            ('Link Options #4', ['Counters', None, None, 'Back-hit'], True),
+            ('Link Options #5', ['Attacks', 'Movement', 'Disable Ki blast', 'No links'], True)
         ])
-        self.u_0a = self.add_hex_entry(self.unknown_page, 'U_0A', max=0xFFFF)
+
